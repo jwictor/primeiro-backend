c975dcb8be316adf858e8873228df7f1385a02b7
Joao Wictor <joaowictoroliveira3301@gmail.com>
2024-12-16 19:53:26 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const router = require("./routers/index");
+
+router(app);
+
+app.listen(port, (error) => {
+    if(error){
+        console.log("Deu erro");
+        return;
+    }
+    console.log("Rodando que e uma belezinha!");
+});
